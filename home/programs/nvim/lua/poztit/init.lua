acb1ed6a751c66d3a88fde1eab2c2b1f7844998e
François Illien <francois@illien.org>
2024-04-28 20:11:07 +0200

    feat(nvim): add system clipboard support

--- home/programs/nvim/lua/poztit/init.lua
+++ home/programs/nvim/lua/poztit/init.lua
@@ -1,7 +1,9 @@
 vim.cmd("language en_US.utf-8")
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.textwidth = 100
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.swapfile = false
 vim.opt.backup = false
+vim.opt.clipboard = 'unnamedplus'
