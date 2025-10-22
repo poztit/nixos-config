949dca7a013a8077d712673cf6bf805a83adae13
François Illien <francois@illien.org>
2025-10-22 17:10:30 +0200

    feat(flake): migrate to nixpkgs unstable and add deploy-rs
    - Switch from nixpkgs-25.05-darwin to nixpkgs-unstable
    - Switch nix-darwin from stable to master branch
    - Add deploy-rs for remote deployment capabilities
    - Add devShell with deploy-rs package
    - Configure deploy target for server host
    - Comment out laptop and desktop configurations
    - Add deployment checks for x86_64-linux
    - Update flake.lock with new dependency versions

--- flake.nix
+++ flake.nix
@@ -2,55 +2,78 @@
   description = "François's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin = {
+      # url = "github:lnl7/nix-darwin/nix-darwin-25.05";
+      url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    darwin = {
-      url = "github:lnl7/nix-darwin/nix-darwin-25.05";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, sops-nix, darwin, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, sops-nix, darwin, home-manager, deploy-rs, ... }:
     let
       pkgs = import nixpkgs {
         config.allowUnfree = true;
       };
     in
-    {
+    rec {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
       formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
+      devShells =
+        let
+          systems = builtins.attrNames deploy-rs.packages;
+          mkShellFor = system:
+            let
+              pkgsFor = nixpkgs.legacyPackages.${system};
+              deployRsPkg = deploy-rs.packages.${system}.deploy-rs or deploy-rs.packages.${system}.default;
+            in
+            pkgsFor.mkShell {
+              packages = [
+                deployRsPkg
+              ];
+            };
+        in
+        nixpkgs.lib.genAttrs systems (system: {
+          default = mkShellFor system;
+        });
       nixosConfigurations = {
-        "laptop" = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./hosts/laptop/default.nix
-            sops-nix.nixosModules.sops
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.fillien = import ./home/default.nix;
-            }
-          ];
-        };
-        "desktop" = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./hosts/desktop/default.nix
-            sops-nix.nixosModules.sops
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.fillien = import ./home/default.nix;
-            }
-          ];
-        };
+        # "laptop" = nixpkgs.lib.nixosSystem {
+        #   modules = [
+        #     ./hosts/laptop/default.nix
+        #     sops-nix.nixosModules.sops
+        #     home-manager.nixosModules.home-manager
+        #     {
+        #       home-manager.useGlobalPkgs = true;
+        #       home-manager.useUserPackages = true;
+        #       home-manager.users.fillien = import ./home/default.nix;
+        #     }
+        #   ];
+        # };
+        # "desktop" = nixpkgs.lib.nixosSystem {
+        #   modules = [
+        #     ./hosts/desktop/default.nix
+        #     sops-nix.nixosModules.sops
+        #     home-manager.nixosModules.home-manager
+        #     {
+        #       home-manager.useGlobalPkgs = true;
+        #       home-manager.useUserPackages = true;
+        #       home-manager.users.fillien = import ./home/default.nix;
+        #     }
+        #   ];
+        # };
         "server" = nixpkgs.lib.nixosSystem {
           modules = [
             ./hosts/server/default.nix
@@ -85,5 +108,14 @@
           ];
         };
       };
+      deploy.nodes."server" = {
+        hostname = "fillien-desktop";
+        sshUser = "root";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.server;
+        };
+      };
+      checks."x86_64-linux" = deploy-rs.lib.x86_64-linux.deployChecks self.deploy;
     };
 }
